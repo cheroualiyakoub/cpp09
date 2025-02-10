3df60d8472bc62ad55fd4529439c7c8646fb0bd1
yacoub cherouali <cheroualiy@gmail.com>
2025-02-10 15:24:31 +0100

    clening

--- ex01/src/RPN.hpp
+++ ex01/src/RPN.hpp
@@ -6,7 +6,7 @@
 /*   By: ycheroua <ycheroua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 14:19:16 by ycheroua          #+#    #+#             */
-/*   Updated: 2025/01/20 15:50:24 by ycheroua         ###   ########.fr       */
+/*   Updated: 2025/02/10 14:56:47 by ycheroua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,24 @@
 #include <stack>
 #include <unordered_map>
 
-class RNP
+class RPN
 {
 	public :
-		RNP(void);
-		RNP(const RNP& copy);
-		~RNP();
-		RNP& operator=(const RNP& copy);
-		void fillOperations(void);
+		RPN(void);
+		RPN( std::string &expression);
+		RPN(const RPN& copy);
+		~RPN();
+		RPN& operator=(const RPN& copy);
+		void calculaiton(void);
 		std::unordered_map<char, std::function<double(double, double)> > _operationFunctions;
+		void isValidExpression();
+		bool isOperationChar(char c);
 		
 	private:
-		std::string _input;
+		std::string _expression;
 		std::stack<int> _stack;
-		
 		void setOperationFuncitons();
+		void doOperaiton(char operation);
 		static double add(double a, double b);
 		static double subtract(double a, double b);
 		static double multiply(double a, double b);
