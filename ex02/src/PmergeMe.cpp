1e3bce9dd387f7e262801771c4185b9d6b111bbb
yacoub cherouali <cheroualiy@gmail.com>
2025-02-12 16:11:51 +0100

    implement sorting gor Vecotr and deque and a tymplate bas function for both of them

--- ex02/src/PmergeMe.cpp
+++ ex02/src/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: ycheroua <ycheroua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 20:12:55 by ycheroua          #+#    #+#             */
-/*   Updated: 2025/02/11 21:40:13 by ycheroua         ###   ########.fr       */
+/*   Updated: 2025/02/12 16:10:44 by ycheroua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,18 @@ PmergeMe::PmergeMe(void)
 PmergeMe::PmergeMe(std::vector<std::string>  rawNumbers) : _rawNumbers(rawNumbers)
 {
 	fillContainerFromRawNumbers(rawNumbers, this->_vecotrNumbers);	
-	fillContainerFromRawNumbers(rawNumbers, this->_listNumbers);	
+	fillContainerFromRawNumbers(rawNumbers, this->_dequeNumbers);
+	std::cout << "before:   ";
+	// printContainer(_vecotrNumbers);
+	// sortVector();
+	std::cout << "After     ";
+	// printContainer(_vecotrNumbers);
+	// sortDeque();
+
+	sortConiatner(_vecotrNumbers);
+	sortConiatner(this->_dequeNumbers);
+	printContainer(_dequeNumbers);
+	printContainer(_vecotrNumbers);
 }
 
 PmergeMe::PmergeMe(const PmergeMe& copy) : _rawNumbers(copy._rawNumbers)
@@ -36,11 +47,10 @@ PmergeMe::~PmergeMe()
 PmergeMe& PmergeMe::operator=(const PmergeMe& copy) 
 {
 	this->_rawNumbers = copy._rawNumbers;
-	this->_listNumbers = copy._listNumbers;
+	this->_dequeNumbers = copy._dequeNumbers;
 	this->_vecotrNumbers = copy._vecotrNumbers;
 	return *this;
 }
-
 
 template <typename Container>
 void PmergeMe::fillContainerFromRawNumbers(const std::vector<std::string>& rawNumbers, Container& container)
@@ -48,9 +58,129 @@ void PmergeMe::fillContainerFromRawNumbers(const std::vector<std::string>& rawNu
     for (typename std::vector<std::string>::const_iterator it = rawNumbers.begin();
          it != rawNumbers.end(); ++it)
     {
-		if (!std::isdigit(it->c_str()))
-			throw std::invalid_argument ("invalid character");
+		size_t i = (it->c_str()[0] == '-') ? 1 : 0;
+    	for (; i < it->size(); ++i)
+		{
+        	if (!std::isdigit(it->c_str()[i])) 
+				throw std::invalid_argument("Invalid input character");
+		}
         int value = std::atoi(it->c_str());
         container.push_back(value);
     }
+}
+
+template <typename Container>
+void PmergeMe::printContainer(Container& container)
+{
+    for (typename Container::const_iterator it = container.begin(); it != container.end(); ++it)
+        std::cout << *it << " ";
+	std::cout <<  "  " << ContainerTypeHelper<Container>::getType();
+	std::cout << std::endl;	
+}
+
+void PmergeMe::sortVector(void)
+{
+    if (_vecotrNumbers.size() < 2)
+        return;
+
+    std::vector<int> largerNumbers;
+    std::vector<int> smallerNumbers;
+    std::vector<int>::iterator it = _vecotrNumbers.begin();
+
+    while (it != _vecotrNumbers.end())
+    {
+        std::vector<int>::iterator next = it + 1;
+
+        if (next != _vecotrNumbers.end())
+        {
+            if (*it > *next)
+                std::swap(*it, *next);
+
+            largerNumbers.push_back(*next); 
+            smallerNumbers.push_back(*it);
+            ++it;
+        }
+        else
+            smallerNumbers.push_back(*it);
+        ++it;
+    }
+    std::sort(largerNumbers.begin(), largerNumbers.end());
+    std::vector<int> sortedVector = largerNumbers;
+    for (size_t i = 0; i < smallerNumbers.size(); ++i)
+    {
+        std::vector<int>::iterator pos = std::lower_bound(sortedVector.begin(), sortedVector.end(), smallerNumbers[i]);
+        sortedVector.insert(pos, smallerNumbers[i]);
+    }
+    _vecotrNumbers = sortedVector;
+}
+
+void PmergeMe::sortDeque(void)
+{
+    if (_dequeNumbers.size() < 2)
+        return;
+    std::deque<int> largerNumbers;
+    std::deque<int> smallerNumbers;
+    std::deque<int>::iterator it = _dequeNumbers.begin();
+    while (it != _dequeNumbers.end())
+    {
+        std::deque<int>::iterator next = it + 1;
+
+        if (next != _dequeNumbers.end())
+        {
+            if (*it > *next)
+                std::swap(*it, *next);
+            largerNumbers.push_back(*next); 
+            smallerNumbers.push_back(*it);
+            ++it;
+        }
+        else
+            smallerNumbers.push_back(*it);
+        ++it;
+    }
+    std::sort(largerNumbers.begin(), largerNumbers.end());
+    std::deque<int> sortedDeque(largerNumbers.begin(), largerNumbers.end());
+    for (size_t i = 0; i < smallerNumbers.size(); ++i)
+    {
+        std::deque<int>::iterator pos = std::lower_bound(sortedDeque.begin(), sortedDeque.end(), smallerNumbers[i]);
+        sortedDeque.insert(pos, smallerNumbers[i]);
+    }
+    _dequeNumbers = sortedDeque;
+}
+
+template <typename Container>
+void PmergeMe::sortConiatner(Container& container)
+{
+    if (container.size() < 2)
+        return;
+
+    Container largerNumbers;
+    Container smallerNumbers;
+    typename Container::iterator it = container.begin();
+    while (it != container.end())
+    {
+        typename Container::iterator next = it + 1;
+
+        if (next != container.end())
+        {
+            if (*it > *next)
+                std::swap(*it, *next);
+
+            largerNumbers.push_back(*next); 
+            smallerNumbers.push_back(*it);
+            ++it;
+        }
+        else
+        {
+            smallerNumbers.push_back(*it);
+        }
+        ++it;
+    }
+    std::sort(largerNumbers.begin(), largerNumbers.end());
+    Container sortedDeque(largerNumbers.begin(), largerNumbers.end());
+    for (size_t i = 0; i < smallerNumbers.size(); ++i)
+    {
+        typename Container::iterator  pos = std::lower_bound(sortedDeque.begin(), sortedDeque.end(), smallerNumbers[i]);
+        sortedDeque.insert(pos, smallerNumbers[i]);
+    }
+    container = sortedDeque;
 }
--- ex02/src/PmergeMe.hpp
+++ ex02/src/PmergeMe.hpp
@@ -6,9 +6,13 @@
 /*   By: ycheroua <ycheroua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 14:19:16 by ycheroua          #+#    #+#             */
-/*   Updated: 2025/02/11 21:29:04 by ycheroua         ###   ########.fr       */
+/*   Updated: 2025/02/12 16:03:55 by ycheroua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+
+#ifndef PMERGEME_HPP
+#define PMERGEME_HPP
 
 #include <iostream>
 #include <exception>
@@ -16,24 +20,57 @@
 #include <unordered_map>
 #include <vector>
 #include <list>
+#include <deque>
 #include <sstream>
 
 class PmergeMe
 {
 	private:
 		std::vector<std::string>  _rawNumbers;
-		std::list<int> _listNumbers;
+		std::deque<int> _dequeNumbers;
 		std::vector<int> _vecotrNumbers;
-		void checkInput();
-		void sortList(void);
-		void sortVector(void);
-		
+
 	public:
 		PmergeMe();
 		PmergeMe(std::vector<std::string> rawNumbers);
 		PmergeMe(const PmergeMe& copy);
 		~PmergeMe();
+		PmergeMe& operator=(const PmergeMe& copy);
+		
 		template <typename Container>
 		void fillContainerFromRawNumbers(const std::vector<std::string>& rawNumbers, Container& container);
-		PmergeMe& operator=(const PmergeMe& copy);
+		template <typename Container>
+		void printContainer(Container& container);
+		template <typename Container>
+		void sortConiatner(Container& container);
+		
+		void sortVector(void);
+		void sortDeque(void);
+
 };
+
+template <typename Container>
+struct ContainerTypeHelper
+{
+	static std::string getType() { return "Unknown Container"; }
+};
+
+template <>
+struct ContainerTypeHelper<std::vector<int> >
+{
+	static std::string getType() { return "std::vector<int>"; }
+};
+
+template <>
+struct ContainerTypeHelper<std::deque<int> >
+{
+	static std::string getType() { return "std::deque<int>"; }
+};
+
+template <>
+struct ContainerTypeHelper<std::list<int> >
+{
+	static std::string getType() { return "std::list<int>"; }
+};
+
+#endif
